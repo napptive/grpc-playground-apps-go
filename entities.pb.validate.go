a4fc3b21c4208d3d4953f48290b5bb06f544a333
hermione-napptive <cicd@napptive.com>
2022-06-16 13:53:10 +0000

    gpm automatic publish

--- entities.pb.validate.go
+++ entities.pb.validate.go
@@ -1091,3 +1091,121 @@ var _ interface {
 	Cause() error
 	ErrorName() string
 } = RemoveApplicationRequestValidationError{}
+
+// Validate checks the field values on ResumeApplicationRequest with the rules
+// defined in the proto definition for this message. If any rules are
+// violated, the first error encountered is returned, or nil if there are no violations.
+func (m *ResumeApplicationRequest) Validate() error {
+	return m.validate(false)
+}
+
+// ValidateAll checks the field values on ResumeApplicationRequest with the
+// rules defined in the proto definition for this message. If any rules are
+// violated, the result is a list of violation errors wrapped in
+// ResumeApplicationRequestMultiError, or nil if none found.
+func (m *ResumeApplicationRequest) ValidateAll() error {
+	return m.validate(true)
+}
+
+func (m *ResumeApplicationRequest) validate(all bool) error {
+	if m == nil {
+		return nil
+	}
+
+	var errors []error
+
+	// no validation rules for TargetEnvironmentQualifiedName
+
+	// no validation rules for AccountId
+
+	// no validation rules for EnvironmentId
+
+	if utf8.RuneCountInString(m.GetApplicationName()) < 1 {
+		err := ResumeApplicationRequestValidationError{
+			field:  "ApplicationName",
+			reason: "value length must be at least 1 runes",
+		}
+		if !all {
+			return err
+		}
+		errors = append(errors, err)
+	}
+
+	if len(errors) > 0 {
+		return ResumeApplicationRequestMultiError(errors)
+	}
+	return nil
+}
+
+// ResumeApplicationRequestMultiError is an error wrapping multiple validation
+// errors returned by ResumeApplicationRequest.ValidateAll() if the designated
+// constraints aren't met.
+type ResumeApplicationRequestMultiError []error
+
+// Error returns a concatenation of all the error messages it wraps.
+func (m ResumeApplicationRequestMultiError) Error() string {
+	var msgs []string
+	for _, err := range m {
+		msgs = append(msgs, err.Error())
+	}
+	return strings.Join(msgs, "; ")
+}
+
+// AllErrors returns a list of validation violation errors.
+func (m ResumeApplicationRequestMultiError) AllErrors() []error { return m }
+
+// ResumeApplicationRequestValidationError is the validation error returned by
+// ResumeApplicationRequest.Validate if the designated constraints aren't met.
+type ResumeApplicationRequestValidationError struct {
+	field  string
+	reason string
+	cause  error
+	key    bool
+}
+
+// Field function returns field value.
+func (e ResumeApplicationRequestValidationError) Field() string { return e.field }
+
+// Reason function returns reason value.
+func (e ResumeApplicationRequestValidationError) Reason() string { return e.reason }
+
+// Cause function returns cause value.
+func (e ResumeApplicationRequestValidationError) Cause() error { return e.cause }
+
+// Key function returns key value.
+func (e ResumeApplicationRequestValidationError) Key() bool { return e.key }
+
+// ErrorName returns error name.
+func (e ResumeApplicationRequestValidationError) ErrorName() string {
+	return "ResumeApplicationRequestValidationError"
+}
+
+// Error satisfies the builtin error interface
+func (e ResumeApplicationRequestValidationError) Error() string {
+	cause := ""
+	if e.cause != nil {
+		cause = fmt.Sprintf(" | caused by: %v", e.cause)
+	}
+
+	key := ""
+	if e.key {
+		key = "key for "
+	}
+
+	return fmt.Sprintf(
+		"invalid %sResumeApplicationRequest.%s: %s%s",
+		key,
+		e.field,
+		e.reason,
+		cause)
+}
+
+var _ error = ResumeApplicationRequestValidationError{}
+
+var _ interface {
+	Field() string
+	Reason() string
+	Key() bool
+	Cause() error
+	ErrorName() string
+} = ResumeApplicationRequestValidationError{}
